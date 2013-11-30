8e3ba9ab6a3b99e919a3801d21d045bff712c175
Roman Vlasenko <ipcreeper@gmail.com>
2013-11-30 11:33:22 +0200

    Added handling for case when site URL is not reachable

--- src/eventPage.js
+++ src/eventPage.js
@@ -1,41 +1,10 @@
 var alarms = chrome.alarms;
-var storage = chrome.storage.sync;
+var runtime = chrome.runtime;
 
 alarms.create("CheckRulesSchedule", {periodInMinutes: 1});
 
 alarms.onAlarm.addListener(function (alarm) {
     if (alarm.name = 'CheckRulesSchedule') {
-        performScheduledChecking();
+        runtime.sendMessage({msg: "timeToCheck"});
     }
 });
-
-function performScheduledChecking() {
-    var somethingChanged = false;
-    var rulesChecked = [];
-
-    storage.get('rules', function (data) {
-        var rules = data.rules;
-
-        _.each(rules, function (rule) {
-            $.get(rule.url, function (data) {
-                var newVal = $(data).find(rule.selector).text().trim();
-
-                if (newVal && newVal != rule.value) {
-                    rule.value = newVal;
-                    somethingChanged = true;
-                }
-
-                rulesChecked.push(rule);
-                if (rulesChecked.length == rules.length && somethingChanged) {
-                    sync(rulesChecked);
-                }
-            });
-        });
-    });
-}
-
-function sync(rules) {
-    storage.set({'rules': rules}, function () {
-        chrome.runtime.sendMessage({msg: "onRulesUpdated"});
-    });
-}
--- src/sourceChecker.js
+++ src/sourceChecker.js
@@ -1,22 +1,28 @@
 var storage = chrome.storage.sync;
+var NOT_AVAILABLE = "Not available";
 
 $(document).ready(function () {
     chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
-            if (request.msg == "onRulesUpdated") {
-                refreshRuleControls();
+            if (request.msg == "timeToCheck") {
+                performScheduledChecking()
             }
         });
 });
 
 function checkAndUpdate(rule) {
-    $.get(rule.url, function (data) {
-        var newVal = $(data).find(rule.selector).text().trim();
-        if (newVal != 'undefined' && newVal != rule.value) {
-            rule.value = newVal;
-            updateRuleValue(rule);
-        }
-    });
+    $.ajax({url: rule.url,
+               success: function (srcHtml) {
+                   var newVal = $(srcHtml).find(rule.selector).text().trim();
+                   if (newVal != 'undefined' && newVal != rule.value) {
+                       rule.value = newVal;
+                       updateRuleValue(rule);
+                   }
+               },
+               error: function () {
+                   rule.value = NOT_AVAILABLE;
+                   updateRuleValue(rule);
+               }});
 }
 
 function updateRuleValue(rule) {
@@ -30,6 +36,40 @@ function updateRuleValue(rule) {
 
         storage.set({'rules': rules}, function () {
             refreshRuleControls();
+        });
+    });
+}
+
+function performScheduledChecking() {
+    var somethingChanged = false;
+    var rulesChecked = [];
+
+    storage.get('rules', function (data) {
+        var rules = data.rules;
+
+        _.each(rules, function (rule) {
+
+            $.ajax({url: rule.url,
+                       success: function (srcHtml) {
+                           var newVal = $(srcHtml).find(rule.selector).text().trim();
+
+                           if (newVal && newVal != rule.value) {
+                               rule.value = newVal;
+                               somethingChanged = true;
+                           }
+
+                           rulesChecked.push(rule);
+                           if (rulesChecked.length == rules.length && somethingChanged) {
+                               refreshRuleControls();
+                           }
+                       },
+                       error: function () {
+                           rule.value = NOT_AVAILABLE;
+                           rulesChecked.push(rule);
+                           if (rulesChecked.length == rules.length && somethingChanged) {
+                               refreshRuleControls();
+                           }
+                       }});
         });
     });
 }
